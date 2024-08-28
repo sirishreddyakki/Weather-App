a449ca5c66383745a430751ebaba215c39dff981
sirishreddyakki <sirishreddyakki@gmail.com>
2024-08-28 22:56:36 +0530

    third

---
+++ src/api.js
@@ -0,0 +1,10 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '046993214amsh3f87f3d6977e502p1b5aefjsn18d3ed2a9eb4',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
